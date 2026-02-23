d32795b43194e2d9803cf13ce3449128d049aed8
2lmf <2lmf.info@gmail.com>
2026-02-23 20:30:34 +0100

    fix: skipWaiting in sw.js to automatically bust cache

--- sw.js
+++ sw.js
@@ -1,18 +1,33 @@
-const CACHE_NAME = 'sharkboat-v1';
+const CACHE_NAME = 'sharkboat-v3';
 const ASSETS = [
     './',
     './index.html',
     './styles.css',
     './app.js',
     './manifest.json',
-    './icon.svg'
+    './icon.svg',
+    './logo.png'
 ];
 
 self.addEventListener('install', (e) => {
+    self.skipWaiting();
     e.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => cache.addAll(ASSETS))
     );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
