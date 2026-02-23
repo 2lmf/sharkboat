a6728d72cc61055cfa1ac1ee3b751213b0e91099
2lmf <2lmf.info@gmail.com>
2026-02-23 19:53:13 +0100

    Add PWA support (manifest, icons, service worker)

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'sharkboat-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './styles.css',
+    './app.js',
+    './manifest.json',
+    './icon.svg'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request)
+            .then((response) => response || fetch(e.request))
+    );
+});
